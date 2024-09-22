affaff26b24c6724be5ff86a318b8fdda9a2bf52
Dopa91 <code.dopatka@gmail.com>
2024-09-23 01:10:18 +0200

    edited button widget

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -4,32 +4,39 @@ class MyNewButton extends StatelessWidget {
   const MyNewButton({
     super.key,
     required this.newText,
+    required this.nextSite,
   });
 
   final String newText;
+  final void Function()? nextSite;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 16, 97, 219),
-        borderRadius: BorderRadius.circular(24),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            newText,
-            style: const TextStyle(
-                fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
-          ),
-          const SizedBox(width: 12),
-          const Icon(
-            Icons.arrow_forward_ios,
-            color: Colors.white,
-          ),
-        ],
+    return GestureDetector(
+      onTap: nextSite,
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 16, 97, 219),
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              newText,
+              style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            const SizedBox(width: 12),
+            const Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.white,
+            ),
+          ],
+        ),
       ),
     );
   }
