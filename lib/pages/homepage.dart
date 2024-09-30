ec9e419d0d8e19a13c9ef187e0165bcacc5a9345
Dopa91 <code.dopatka@gmail.com>
2024-09-30 23:13:20 +0200

    cleaned up, extr. widgets

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
-// import 'package:table_calendar/table_calendar.dart';
+import 'package:learning_app_projekt_uebung/pages/first_calendar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -45,38 +44,6 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class FirstCalendar extends StatelessWidget {
-  const FirstCalendar({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Text(
-          "Dein Persönlicher Terminmanager",
-          style: TextStyle(
-            fontWeight: FontWeight.w800,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            decoration: BoxDecoration(
-                color: Colors.grey, borderRadius: BorderRadius.circular(24)),
-            child: TableCalendar(
-              focusedDay: DateTime.now(),
-              firstDay: DateTime.utc(2023, 10, 16),
-              lastDay: DateTime.utc(2030, 3, 14),
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
