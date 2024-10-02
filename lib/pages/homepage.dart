34c094995f6349d3946e0311b13f2ad93477d777
Dopa91 <code.dopatka@gmail.com>
2024-10-03 01:51:54 +0200

    new functions NavigatorBar

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,53 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:learning_app_projekt_uebung/pages/first_calendar.dart';
+import 'package:learning_app_projekt_uebung/pages/homepage_screen.dart';
+import 'package:learning_app_projekt_uebung/pages/user.dart';
+import 'package:learning_app_projekt_uebung/pages/user_list.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+int currentIndex = 0;
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: NavigationBar(destinations: const [
-        NavigationDestination(icon: Icon(Icons.home), label: "Homepage"),
-        NavigationDestination(icon: Icon(Icons.book), label: "Diary"),
-        NavigationDestination(icon: Icon(Icons.settings), label: "Settings"),
-      ]),
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (value) {
+          setState(() {});
+        },
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: "Homepage"),
+          NavigationDestination(icon: Icon(Icons.book), label: "Diary"),
+          NavigationDestination(icon: Icon(Icons.settings), label: "Settings"),
+        ],
+      ),
       backgroundColor: const Color.fromARGB(255, 217, 192, 242),
-      body: const SafeArea(
-        child: Column(
-          children: [
-            Center(
-              child: Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Row(
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Icon(
-                          Icons.face_sharp,
-                          size: 96,
-                        ),
-                        Text(
-                          "Willkommen,",
-                          style: TextStyle(fontSize: 24),
-                        ),
-                        Text(
-                          "Mina",
-                          style: TextStyle(fontSize: 24),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 24),
-              child: FirstCalendar(),
-            ),
-          ],
-        ),
+      body: HomepageScreen(
+        user: userList[currentIndex],
       ),
     );
   }
