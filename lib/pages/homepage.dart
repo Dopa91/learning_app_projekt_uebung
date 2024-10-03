b8057049db0feb54d90b2bcbcf9a5f6cf8432dc3
Dopa91 <code.dopatka@gmail.com>
2024-10-03 03:03:18 +0200

    edited NavigatonBar

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:learning_app_projekt_uebung/pages/first_calendar.dart';
 import 'package:learning_app_projekt_uebung/pages/homepage_screen.dart';
+import 'package:learning_app_projekt_uebung/pages/settings_screen.dart';
 import 'package:learning_app_projekt_uebung/pages/user.dart';
 import 'package:learning_app_projekt_uebung/pages/user_list.dart';
 
@@ -11,14 +12,24 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-int currentIndex = 0;
-
 class _HomePageState extends State<HomePage> {
+  final List<Widget> widgets = [
+    HomepageScreen(
+      user: userList[2],
+    ),
+    const DiaryScreen(),
+    const SettingsScreen(),
+  ];
+
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (value) {
+        selectedIndex: currentIndex,
+        onDestinationSelected: (index) {
+          currentIndex = index;
           setState(() {});
         },
         destinations: const [
@@ -28,8 +39,29 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       backgroundColor: const Color.fromARGB(255, 217, 192, 242),
-      body: HomepageScreen(
-        user: userList[currentIndex],
+      body: widgets[currentIndex],
+    );
+  }
+}
+
+class DiaryScreen extends StatelessWidget {
+  const DiaryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        forceMaterialTransparency: true,
+        automaticallyImplyLeading: false,
+      ),
+      backgroundColor: const Color.fromARGB(255, 217, 192, 242),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Your Diary"),
+          ],
+        ),
       ),
     );
   }
