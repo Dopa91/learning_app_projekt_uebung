25f45fecaf306b0dc2de808807bb02e492485689
Dopa91 <code.dopatka@gmail.com>
2024-10-04 01:22:44 +0200

    moved to other directory

--- lib/pages/diary_entry.dart
+++
@@ -1,7 +0,0 @@
-class DiaryEntry {
-  String title;
-  String content;
-  DateTime date;
-
-  DiaryEntry({required this.title, required this.content, required this.date});
-}
--- lib/pages/user_list.dart
+++
@@ -1,7 +0,0 @@
-import 'package:learning_app_projekt_uebung/pages/user.dart';
-
-final List<User> userList = [
-  User(name: "Mina"),
-  User(name: "Leonie"),
-  User(name: "Andreas")
-];
