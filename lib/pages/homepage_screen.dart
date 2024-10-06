e52ad19714e3ee91628d926ad946b3991f4fff0c
Dopa91 <code.dopatka@gmail.com>
2024-10-06 19:47:01 +0200

    added pack ImageSlider, added Images

--- lib/pages/homepage_screen.dart
+++ lib/pages/homepage_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
 import 'package:learning_app_projekt_uebung/components/first_calendar.dart';
 import 'package:learning_app_projekt_uebung/components/user.dart';
 
@@ -19,18 +20,42 @@ class HomepageScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
               child: Row(
                 children: [
-                  CircleAvatar(
+                  const CircleAvatar(
                     radius: 72,
                     backgroundImage: AssetImage("assets/images/bober.jpg"),
                   ),
-                  Spacer(),
-                  CircleAvatar(
-                    radius: 96,
-                    backgroundImage: AssetImage("assets/images/niceOMG.gif"),
+                  const Spacer(),
+                  ImageSlideshow(
+                    indicatorRadius: 0,
+                    autoPlayInterval: 4200,
+                    indicatorColor: Colors.purple,
+                    onPageChanged: (value) {
+                      debugPrint("Next Page: $value");
+                    },
+                    isLoop: true,
+                    width: 180,
+                    children: [
+                      Image.asset(
+                        "assets/images/bluesky.jpg",
+                        fit: BoxFit.fitWidth,
+                      ),
+                      Image.asset(
+                        "assets/images/sunnyplace.jpg",
+                        fit: BoxFit.fitWidth,
+                      ),
+                      Image.asset(
+                        "assets/images/flowerbird.jpg",
+                        fit: BoxFit.fitWidth,
+                      ),
+                      Image.asset(
+                        "assets/images/snowplace.jpg",
+                        fit: BoxFit.fitWidth,
+                      ),
+                    ],
                   ),
                 ],
               ),
